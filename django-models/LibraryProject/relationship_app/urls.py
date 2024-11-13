3212cf565bfe9dd8fa46be5fa24768e3b27bb93e
n-asfaw <nardosasfaw77@gmail.com>
2024-11-13 15:09:39 +0300

    third task

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,8 +4,19 @@ from django.urls import path
 from . import views
 from .views import list_books
 from .views import LibraryDetailView
+from .views import CustomLoginView, CustomLogoutView, RegisterView
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),  # URL for the function-based view
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # URL for the class-based view
+     path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+
 ]
+
+
+
+
+
+   
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,12 @@ from django.views.generic import DetailView
 from django.views.generic.detail import DetailView
 from .models import Book,
 from .models import Library  # Import the models here
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import View
+from django.shortcuts import render, redirect
+
 
 # Function-based view to list all books
 def list_books(request):
@@ -21,3 +27,25 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['books'] = self.object.books.all()
         return context
+
+
+# Login view using Django's built-in LoginView
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+# Logout view using Django's built-in LogoutView
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+# Registration view using Django's UserCreationForm
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'relationship_app/register.html', {'form': form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to login after successful registration
+        return render(request, 'relationship_app/register.html', {'form': form})
