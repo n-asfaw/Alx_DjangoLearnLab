33161e2635bb351f19a6745a66d958ab33615fc3
n-asfaw <nardosasfaw77@gmail.com>
2024-11-13 16:59:35 +0300

    third task

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import View
 from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth import logout
 
 
 # Function-based view to list all books
