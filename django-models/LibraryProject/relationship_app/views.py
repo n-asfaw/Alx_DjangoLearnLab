fe33d96a5edcfaecbeefb86e402a4bf7cb4ca73c
n-asfaw <nardosasfaw77@gmail.com>
2024-11-14 10:39:32 +0300

    task 4

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -107,3 +107,35 @@ def delete_book(request, book_id):
         book.delete()
         return redirect('book_list')
     return render(request, 'confirm_delete.html', {'book': book})    
+
+
+@permission_required('relationship_app.can_add_book')  # Update with correct app permission
+def add_book(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'add_book.html', {'form': form})
+
+@permission_required('relationship_app.can_change_book')  # Update with correct app permission
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'edit_book.html', {'form': form})
+
+@permission_required('relationship_app.can_delete_book')  # Update with correct app permission
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'confirm_delete.html', {'book': book})    
