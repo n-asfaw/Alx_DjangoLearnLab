8265c9fbe7ea8015fc70b2eadacc490cbc31f46a
n-asfaw <nardosasfaw77@gmail.com>
2024-12-06 11:35:15 +0300

    imporetd IsAuthenticated

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 # ListView: Retrieves all books
 class BookListView(generics.ListCreateAPIView):
