eaa2d70c70129a6eed5968f91ff65352877e52d0
n-asfaw <nardosasfaw77@gmail.com>
2024-12-06 11:28:30 +0300

    update and delete added

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),  # List and create
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Retrieve
     path('books/create/', BookCreateView.as_view(), name='book-create'),  # Create
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),  # Update
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete
+    # Update an existing book
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),  
+    # Delete a book
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  
 ]
