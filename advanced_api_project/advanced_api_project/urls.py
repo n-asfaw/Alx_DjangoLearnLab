fa4abc17b6918ab62201489b1905ab36d2823fca
n-asfaw <nardosasfaw77@gmail.com>
2024-12-06 11:04:38 +0300

    issue fixed

--- advanced_api_project/advanced_api_project/urls.py
+++ advanced_api_project/advanced_api_project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include  # Import 'include' here
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('api/', include('api.urls')),  # Include API urls
 ]
