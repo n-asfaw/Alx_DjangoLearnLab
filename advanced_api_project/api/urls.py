2900c65d7628451bb373c0e6b9eaa2153188dc46
n-asfaw <nardosasfaw77@gmail.com>
2024-12-06 10:58:33 +0300

    issue fixed

---
+++ advanced_api_project/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    BookListView,
+    BookDetailView,
+    BookCreateView,
+    BookUpdateView,
+    BookDeleteView
+)
+
+urlpatterns = [
+    path('books/', BookListView.as_view(), name='book-list'),  # List and create
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Retrieve
+    path('books/create/', BookCreateView.as_view(), name='book-create'),  # Create
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),  # Update
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete
+]
