1bdfc4bda050f198a6d726372742e5f44912fa8e
n-asfaw <nardosasfaw77@gmail.com>
2024-12-10 19:14:01 +0300

    Adding Comment Functionality to Blog Posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -12,6 +12,13 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.views.generic.edit import UpdateView
 from .forms import CustomUserCreationForm
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .models import Post, Comment
+from .forms import CommentForm
+
 
 
 class PostListView(ListView):
@@ -104,3 +111,138 @@ class ProfileUpdateView(UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()  # Add the comment form to the context
+        context['comments'] = Comment.objects.filter(post=self.object)  # Display all comments for the post
+        return context
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Set the current user as the author
+        form.instance.post = Post.objects.get(id=self.kwargs['post_id'])  # Link the comment to the post
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})
+
+class CommentUpdateView(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_form.html'
+
+    def form_valid(self, form):
+        comment = self.get_object()
+        if comment.author != self.request.user:
+            return redirect('post-detail', pk=comment.post.pk)  # Redirect if user is not the comment author
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})
+
+class CommentDeleteView(LoginRequiredMixin, DeleteView):
+    model = Comment
+    template_name = 'blog/comment_confirm_delete.html'
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})
+
+    def dispatch(self, request, *args, **kwargs):
+        comment = self.get_object()
+        if comment.author != request.user:
+            return redirect('post-detail', pk=comment.post.pk)  # Prevent deleting comments by non-authors
+        return super().dispatch(request, *args, **kwargs)
+class CommentUpdateView(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_form.html'
+
+    def form_valid(self, form):
+        comment = self.get_object()
+        if comment.author != self.request.user:
+            return redirect('post-detail', pk=comment.post.pk)  # Redirect if user is not the comment author
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})
+
+class CommentDeleteView(LoginRequiredMixin, DeleteView):
+    model = Comment
+    template_name = 'blog/comment_confirm_delete.html'
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})
+
+    def dispatch(self, request, *args, **kwargs):
+        comment = self.get_object()
+        if comment.author != request.user:
+            return redirect('post-detail', pk=comment.post.pk)  # Prevent deleting comments by non-authors
+        return super().dispatch(request, *args, **kwargs)
+
+# blog/views.py
+
+# View to show post details, including comments
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()  # Add a form to post a new comment
+        context['comments'] = Comment.objects.filter(post=self.object)  # Fetch comments for this post
+        return context
+
+# View to create a new comment
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Automatically set the current logged-in user as the author
+        form.instance.post = Post.objects.get(id=self.kwargs['post_id'])  # Get the post from the URL
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})  # Redirect to the post detail page after success
+
+# View to update a comment (only by the comment's author)
+class CommentUpdateView(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/comment_form.html'
+
+    def form_valid(self, form):
+        comment = self.get_object()
+        if comment.author != self.request.user:
+            return redirect('post-detail', pk=comment.post.pk)  # Redirect if the user is not the author
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})  # Redirect back to post detail after update
+
+# View to delete a comment (only by the comment's author)
+class CommentDeleteView(LoginRequiredMixin, DeleteView):
+    model = Comment
+    template_name = 'blog/comment_confirm_delete.html'
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})  # Redirect to post detail after deletion
+
+    def dispatch(self, request, *args, **kwargs):
+        comment = self.get_object()
+        if comment.author != request.user:
+            return redirect('post-detail', pk=comment.post.pk)  # Redirect if the user is not the author of the comment
+        return super().dispatch(request, *args, **kwargs)
+
