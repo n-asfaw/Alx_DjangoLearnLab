57e063c373c9f68c95a65fcb24e19fb74d165a3f
n-asfaw <nardosasfaw77@gmail.com>
2024-12-26 11:16:11 +0300

    Serializers edited

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -11,10 +11,8 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        # Create user with the provided data
-        user = User(**validated_data)
-        user.set_password(validated_data['password'])  # Hash the password
-        user.save()
+        # Use get_user_model().objects.create_user to create a new user
+        user = get_user_model().objects.create_user(**validated_data)
         # Create and return a token for the new user
         Token.objects.create(user=user)
         return user
