bcca7be464d0bb291af89bcc6e643b5d0b89c95f
n-asfaw <nardosasfaw77@gmail.com>
2024-12-27 11:22:46 +0300

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,24 +1,27 @@
-from rest_framework import permissions, viewsets
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions
+from rest_framework.response import Response
 
 User = get_user_model()
 
-class UserFollowViewSet(viewsets.ViewSet):
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
     permission_classes = [permissions.IsAuthenticated]
 
-    def follow_user(self, request, pk=None):
+    def get(self, request, *args, **kwargs):
+        users = self.get_queryset()
+        user_data = [{'id': user.id, 'email': user.email} for user in users]
+        return Response(user_data)
+
+class UserFollowView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, pk):
         user_to_follow = get_object_or_404(User, pk=pk)
         request.user.following.add(user_to_follow)
         return Response({'status': 'following'})
 
-    def unfollow_user(self, request, pk=None):
+    def delete(self, request, pk):
         user_to_unfollow = get_object_or_404(User, pk=pk)
         request.user.following.remove(user_to_unfollow)
         return Response({'status': 'unfollowed'})
-
-    def list_following(self, request):
-        following = request.user.following.all()
-        # Serialize the following users here if needed
-        return Response({'following': [user.id for user in following]})
