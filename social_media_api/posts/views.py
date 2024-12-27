77ba9fd6e05da9217cd915b91f0a91e295d83fbd
n-asfaw <nardosasfaw77@gmail.com>
2024-12-27 11:35:38 +0300

    Implementing User Follows and Feed Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,13 +1,17 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics, viewsets, permissions, filters
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from .models import Post
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class UserFeedView(generics.ListAPIView):
-    serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    def get_queryset(self):
-        user = self.request.user
-        return Post.objects.filter(author__in=user.following.all()).order_by('-created_at')
+    def get(self, request, *args, **kwargs):
+        following_users = request.user.following.all()  # Get the users that the current user follows
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')  # Fetch posts from followed users
+        # You can serialize and return the posts here
+        post_data = [{'id': post.id, 'title': post.title, 'content': post.content, 'author': post.author.id} for post in posts]
+        return Response(post_data)
